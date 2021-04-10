1edf7b9996b16c9c4093291ac4f99d6949796398
PrathamRawat <prathamrawat815@gmail.com>
2021-04-09 21:47:51 -0400

    Fix compiler errors for individual auction contract

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.0;
 
 import "./nft-contract.sol";
 
@@ -7,9 +7,9 @@ contract Bid {
     address bidder;
     uint256 bid;
 
-    constructor (address bidder, uint256 bid) {
-        this.bidder = bidder;
-        this.bid = bid;
+    constructor (address bidderAccount, uint256 bidAmount) public {
+        bidder = bidderAccount;
+        bid = bidAmount;
     }
 }
 
@@ -24,69 +24,75 @@ contract Auction is IERC721Receiver {
     Bid[] bids;
 
     uint256 maxBid;
-    address maxBidder;
+    address payable maxBidder;
 
-    MintableToken tokenContract = "0xBe99Fb75bD331387958019Efe30C0F0Aa78D5DbD";
+    MintableToken tokenContract = MintableToken(0xBe99Fb75bD331387958019Efe30C0F0Aa78D5DbD);
 
-    constructor (address owner, uint256 startingBid, uint256 reservePrice, uint256 endBlock, uint256 tokenId) {
-        _registerInterface(IERC721Receiver.onERC721Received.selector);
+    constructor (address sellerAccount, uint256 startingBidAmount, uint256 reservePriceAmount, uint256 endBlockLevel, uint256 nftTokenId) public {
+        // _registerInterface(IERC721Receiver.onERC721Received.selector);
 
-        this.seller = seller;
-        this.startingBid = startingBid;
-        this.reservePrice = reservePrice;
-        this.endBlock = endBlock;
-        this.open = true;
-        tokenContract.safeTransferFrom();
+        seller = sellerAccount;
+        startingBid = startingBidAmount;
+        reservePrice = reservePriceAmount;
+        endBlock = endBlockLevel;
+        maxBid = startingBid;
+        maxBidder = address(0);
+        tokenId = nftTokenId;
+        open = true;
+        // tokenContract.safeTransferFrom();
     }
 
-    function getMaxBid() public view {
+    function getMaxBid() public view returns (uint256) {
         return maxBid;
     }
 
-    function getMaxBidder() public view {
+    function getMaxBidder() public view returns (address){
         return maxBidder;
     }
 
-    function getStartingPrice() public view {
-        return startingPrice;
+    function getStartingPrice() public view returns (uint256){
+        return startingBid;
     }
 
-    function getEndBlock() public view {
+    function getEndBlock() public view returns (uint256){
         return endBlock;
     }
 
-    function getBids() public view {
+    function getBids() public view returns (Bid[] memory){
         return bids;
     }
 
-    function isAuctionFinished() {
+    function isAuctionFinished() public view returns (bool){
         return endBlock >= block.number;
     }
 
-    function cancelAuction() public onlyOwner {
-        require(this.open == true, "This auction is already closed.");
+    function cancelAuction() public {
+        require(open, "This auction is already closed.");
+        require(msg.sender == seller, "Only the seller can close this auction");
         require(msg.sender != address(0));
-        this.open = false;
-        this.maxBidder.transfer(this.maxBid);
+        open = false;
+        if(maxBidder != address(0)) {
+            maxBidder.transfer(maxBid);
+        }
     }
 
     function bid() public payable {
-        require(this.open == true, "This auction has been closed.");
-        require(msg.value > this.maxBid, "Your bid should be greater than the maximum bid.");
+        require(open, "This auction has been closed.");
+        require(msg.value > maxBid, "Your bid should be greater than the maximum bid.");
         require(msg.sender != address(0));
-        require(block.number >= this.endBlock, "This auction has expired.");
-//        Add more error cases
-        this.maxBidder.transfer(this.maxBid); // Returns old bid back to the previous bidder
-        this.maxBid = msg.value;
-        this.maxBidder = msg.sender;
-        this.bids.push(new Bid(msg.sender, msg.value));
+        require(block.number >= endBlock, "This auction has expired.");
+        //        Add more error cases
+        maxBidder.transfer(maxBid); // Returns old bid back to the previous bidder
+        maxBid = msg.value;
+        maxBidder = msg.sender;
+        bids.push(new Bid(msg.sender, msg.value));
     }
 
     function transferItems() public {
-        require(msg.sender == this.owner || msg.sender == this.maxBidder, "Operation not permitted.");
-        require(this.open == true, "This auction has been closed by the owner.");
-        require(block.number > this.endBlock, "This auction has not ended yet.");
-        this.maxBidder.transfer(this.maxBid);
-        tokenContract.safeTransferFrom(this, this.maxBidder, this.tokenId);
+        require(msg.sender == seller || msg.sender == maxBidder, "Operation not permitted.");
+        require(open, "This auction has been closed by the owner.");
+        require(block.number > endBlock, "This auction has not ended yet.");
+        maxBidder.transfer(maxBid);
+        tokenContract.safeTransferFrom(address(this), maxBidder, tokenId);
     }
 }
